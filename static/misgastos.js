1d0b2e9d6a798970b2e94815d0465f921224c3a9
zodman <zodman@c182.vz14.my-virtual-panel.com>
2011-03-07 19:02:01 -0600

    editing

--- static/misgastos.js
+++ static/misgastos.js
@@ -5,6 +5,20 @@ function Agregar(url){
 function editar(url){
     document.location.href=url;
 }
+
+function get_buttons(url){
+return    buttons = {
+        "Si": function(){
+        document.location.href=url;  
+        },
+        "Cancelar": function(){
+            $(this).dialog("close");
+        }
+    }
+
+
+}
+
 function borrar(url){
     buttons = {
         "Si": function(){
@@ -30,4 +44,23 @@ $(document).ready(function(){
         title:"Borrar"
     });
 
+    $("#change_month_number").dialog({
+            autoOpen:false,title:"Cambiar Numero de meses a mostrar"});
 });
+
+function change_number_months(url){
+    buttons = {
+        "Si": function(){
+            $.get(url,{ "number":$("#id_number_month").val()});
+            location.reload();
+        },
+        "No": function(){
+            $(this).dialog("close");
+        }
+    }
+
+
+
+    $("#change_month_number").dialog("option","buttons",buttons);
+    $("#change_month_number").dialog("open");
+}
